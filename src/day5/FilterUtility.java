b1b21a21e79493290263b85d8bf439a45265481a
unknown <test@L-MAA-25004262.paypalcorp.com>
2015-07-24 15:42:28 +0530

    Added the Regex matching capability

--- src/day5/FilterUtility.java
+++ src/day5/FilterUtility.java
@@ -1,51 +1,89 @@
-
-
+package day5;
 
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class FilterUtility
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		HashMap<String,Integer> hm= new HashMap<String,Integer>();
-		hm.put("hello",12);
-		hm.put("hellboy",13);
-		hm.put("asdheat",15);
-		
-	 Map<String,Integer> retMap=	filterAndAddConstraintGap(hm,'h','l',2);
-	 
-     Set<String> wordSet =retMap.keySet();
-     for( String s:wordSet){
-     	System.out.println(s+" "+retMap.get(s));
-     }
+public class FilterUtility {
+	public static final char STAR = '*';
+	public static final char DOT = '.';
+
+	public static void main(String[] args) {
+
+		String regex = "b..n";
+		String candidate = "born";
+		regex = FilterUtility.STAR + regex + FilterUtility.STAR;
+		System.out.println(matchUserRequest(regex, candidate));
+
 	}
-	
-	
-public static Map<String,Integer> filterAndAddConstraintGap( HashMap<String,Integer> mapOfPossibleWordsWithScores, char first, char second, int gap){
 
-     Map<String,Integer> retMap= new HashMap<String,Integer>();
-     Set<String> wordsSet =mapOfPossibleWordsWithScores.keySet();
-	 for(String word:wordsSet ){
-	     int i=0;
-	     for(char c:word.toCharArray()){
-		     if(c==first){
-			     if(word.length()>i+gap+1){
-				    if(word.charAt(i+gap+1)==second)
-					  retMap.put(word,mapOfPossibleWordsWithScores.get(word));
-				  }
-				
-			   }
-		 
-		     i++;
-		 }
-	 
-	 }
+	public static Map<String, Integer> filterAndAddConstraintGap(
+			HashMap<String, Integer> mapOfPossibleWordsWithScores, char first,
+			char second, int gap) {
 
+		Map<String, Integer> retMap = new HashMap<String, Integer>();
+		Set<String> wordsSet = mapOfPossibleWordsWithScores.keySet();
+		for (String word : wordsSet) {
+			int i = 0;
+			for (char c : word.toCharArray()) {
+				if (c == first) {
+					if (word.length() > i + gap + 1) {
+						if (word.charAt(i + gap + 1) == second)
+							retMap.put(word,
+									mapOfPossibleWordsWithScores.get(word));
+					}
 
-return retMap;
-}
+				}
+
+				i++;
+			}
+
+		}
+
+		return retMap;
+	}
+
+	public static boolean matchUserRequest(String regx, String candidate) {
+		if (regx.isEmpty()) {
+			return false;
+		} else {
+			if (regx.charAt(0) == STAR) {
+				if (regx.length() == 1) {
+					return true;
+				} else {
+					return matchStar(regx.substring(1), candidate);
+				}
+			}
+
+			else if (candidate.isEmpty()) {
+				return false;
+			} else if (regx.charAt(0) == DOT
+					|| regx.charAt(0) == candidate.charAt(0)) {
+
+				if (regx.length() == 1 && candidate.length() == 1) {
+					return true;
+				}
+
+				else {
+					return matchUserRequest(regx.substring(1),
+							candidate.substring(1));
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+
+	private static boolean matchStar(String regx, String candidate) {
+		for (int i = 0; i < candidate.length(); i++) {
+			if (matchUserRequest(regx, candidate.substring(i))) {
+				return true;
+			} else {
+				continue;
+			}
+		}
+
+		return false;
+	}
 }
