39d54eccea0c8ef6810c67e71302b0ed547d959b
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-24 14:46:15 +0530

    created Scrabble.java

---
+++ Scrabble.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class AnagramListStructure{	
+	List<String> anagramList;
+	int score;
+	
+	AnagramListStructure(String word)
+	{
+		score = calculateScore(word);
+		anagramList = new ArrayList<String>();
+		anagramList.add(word);
+	}
+}
+class ScrabbleHelper
+{
+	Map <String, AnagramListStructure> anagramMap;
+	
+	ScrabbleHelper()
+	{
+		Map <String, AnagramListStructure> anagramMap = new HashMap<String, AnagramListStructure>();
+		File file = new File("sowpods.txt");
+		if (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+			String word;
+			String key = calculateKey(word);
+			AnagramListStructure anagramListStructure;
+			if(anagramMap.containsKey(word)){
+				anagramListStructure = anagramMap.get(key);
+				anagramListStructure.anagramList.add(word);
+			}
+			else{
+				anagramListStructure = new AnagramListStructure(word);
+				anagramMap.put(key, anagramListStructure);
+			}
+		}
+	}
+	
+	String calculateKey(String word)
+	{
+        char[] charsInWord = original.toCharArray();
+        Arrays.sort(charsInWord);
+        String sortedWord = new String(charsInWord);
+        return sortedWord;
+		
+	}
+}
+
+public class Scrabble{
+		public static void main(String[] args)
+		{
+			
+		}
+}
