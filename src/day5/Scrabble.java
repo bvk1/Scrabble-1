56faaccd892f9069f298cfa87fa59dc794e53de1
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-24 15:35:14 +0530

    created instance of scrabble helper

--- src/day5/Scrabble.java
+++ src/day5/Scrabble.java
@@ -1,5 +1,19 @@
 package day5;
 
-public class Scrabble {
+import java.util.*;
+import java.io.*;
 
+public class Scrabble {
+  public static void main(String[] args)
+  {
+    ScrabbleHelper scrabbleHelper = new ScrabbleHelper();
+    Map <String, AnagramListStructure> anagramMap = scrabbleHelper.getMap();
+    while(1)
+    {
+      System.out.println("Enter the word");
+      Scanner scanner = new Scanner(System.in);
+      String word = scanner.nextLine();
+      
+    }
+  }
 }
